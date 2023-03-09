bd0ea22d30677ed9bfc17510882ef1f12932f21d
leozizz <leonardocps2015@gmail.com>
2023-03-09 11:45:43 -0300

    Ajustes na estrutura e inserção de estilos

--- src/LandingPage/index.js
+++ src/LandingPage/index.js
@@ -3,20 +3,23 @@ import './index.css'
 
 export default function LandingPage() {
     return (
-        <div>
+        <div className="modo-claro">
             <header className="header">
                 <div className="header-container limitar-largura">
-                    <img className="Logo" src="./assets/img/barbearia-logo.png"></img>
-                    <button className="botao"><img src="./assets/img/moon.png"></img>Dark</button>
+                    <img className="logo" src="./assets/img/barbearia-logo.png" alt="logo"/>
+                    <button className="botao"><img src="./assets/img/moon.png" alt="icone"/>Dark</button>
                 </div>
             </header>
 
             <section className="banner"></section>
-            <section className="content">
-                <h1>Bem-vindo a Barber Shop</h1>
-                <p>Nossa barbearia sempre oferece profissionais de qualidade e estamos prontos para lidar com suas maiores expectativas.</p>
-                <p>Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos uma equipe premiada que demonstrou o talento de mestres barbeiros em vários concursos de estilo. Deixe nosso barbeiro ser seu estilista pessoal e você nunca ficará desapontado.</p>
-                <p className="assinatura">S. Kelly</p>
+
+            <section className="content limitar-largura">
+                <div className="limitar-largura">
+                    <h1>Bem-vindo a Barber Shop</h1>
+                    <p>Nossa barbearia sempre oferece profissionais de qualidade e estamos prontos para lidar com suas maiores expectativas.</p>
+                    <p>Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos uma equipe premiada que demonstrou o talento de mestres barbeiros em vários concursos de estilo. Deixe nosso barbeiro ser seu estilista pessoal e você nunca ficará desapontado.</p>
+                    <p className="assinatura">S. Kelly</p>
+                </div>
             </section>
         </div>
     )
